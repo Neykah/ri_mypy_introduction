675176f9a9230f22a9b6b404cea62b11251fb1b6
Morgan Lefranc <mlefranc@ridge-i.com>
2022-01-27 14:39:29 +0900

    Modifications

--- python_none_problem/bar.py
+++ python_none_problem/bar.py
@@ -8,13 +8,19 @@ class Bar:
     You MUST call `.initialize_system()` before formatting your foo!
     """
 
-    my_foo: Optional[Foo]
+    # my_foo: Optional[Foo]
 
-    def __init__(self):
-        self.my_foo = None
+    def __init__(self) -> None:
+        self.my_foo: Optional[Foo] = None
 
     def initialize_system(self, foo: Foo) -> None:
         self.my_foo = foo
 
     def format_foo(self) -> str:
+        assert self.my_foo is not None, "My foo is not initialized!"
         return f"My foo's x is {self.my_foo.x}!"
+
+
+def main() -> None:
+    bar = Bar()
+    bar.format_foo()
--- python_none_problem/foo_example.py
+++ python_none_problem/foo_example.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Optional
 
 
 @dataclass(frozen=True)
@@ -10,17 +11,18 @@ class FooException(Exception):
     ...
 
 
-def get_a_foo(foo_id):
+def get_a_foo(foo_id: int) -> Optional[Foo]:
     if foo_id > 100:
         return Foo(foo_id)
     return None
 
 
-def do_something_with(a_foo):
+def do_something_with(a_foo: Foo) -> int:
     return a_foo.x
 
 
-def main():
+def main() -> None:
     foo = get_a_foo(123)
-    do_something_with(foo)
+    if foo is not None:
+        do_something_with(foo)
     print("Success")
